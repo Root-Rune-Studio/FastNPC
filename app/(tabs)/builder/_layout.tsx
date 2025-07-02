1dda5fc7a5466bf612b4367f1e436cd56ea75b7e
Michael Nunan <michael.nunan@outlook.com>
2025-07-01 22:29:55 -0500

    added SafeAreaView component to detect ios and android device safe insets

--- app/(tabs)/builder/_layout.tsx
+++ app/(tabs)/builder/_layout.tsx
@@ -1,14 +1,17 @@
 import { Slot, useSegments } from 'expo-router';
 import { ThemedView } from '@/components/ThemedView';
 import BuildStatus from '@/components/build-ui/BuildStatus';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function BuildLayout() {
   const segments = useSegments();
   const routeName = segments[segments.length - 1] || '';
   return (
-    <ThemedView style={{ flex: 1, top: 25 }}>
-      <BuildStatus routeName={routeName} />
-      <Slot />
+    <ThemedView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <BuildStatus routeName={routeName} />
+        <Slot />
+      </SafeAreaView>
     </ThemedView>
   );
 }
