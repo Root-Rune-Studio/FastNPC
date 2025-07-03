8a0ecf3b442724dbaa4043643c0dfbd68c3ab30a
Michael Galo <michael@michaelthinks.co>
2025-07-03 18:45:24 -0500

    removed the paralax view

--- app/(tabs)/builder/build-step-1.tsx
+++ app/(tabs)/builder/build-step-1.tsx
@@ -1,35 +1,22 @@
-import { StyleSheet } from 'react-native';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
+import { ScrollView, StyleSheet } from 'react-native';
 
 export default function BuildStep1() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
+    <ScrollView style={styles.container}>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Build Step 1</ThemedText>
       </ThemedView>
-      <ThemedText>This is where we&#39;re looking to add Gender, Age, and Potency.</ThemedText>
-    </ParallaxScrollView>
+      <ThemedText>This is where we're looking to add Gender, Age, and Potency.</ThemedText>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+  container: {
+    flex: 1,
+    padding: 16,
   },
   titleContainer: {
     flexDirection: 'row',
